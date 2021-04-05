ac4e6fb5d6db180b9aa95a15a39a58bcee25e620
Hasibur_Rahman_Bijoy <71169981+HasiburBijoy007@users.noreply.github.com>
2021-04-05 18:03:10 +0600

    Add files via upload

---
+++ Exercise_06/SVMfull.m
@@ -0,0 +1,317 @@
+function [model] = svmTrain(X, Y, C, kernelFunction, ...
+                            tol, max_passes)
+
+
+
+if ~exist('tol', 'var') || isempty(tol)
+    tol = 1e-3;
+end
+
+if ~exist('max_passes', 'var') || isempty(max_passes)
+    max_passes = 5;
+end
+
+% Data parameters
+m = size(X, 1);
+n = size(X, 2);
+
+% Map 0 to -1
+Y(Y==0) = -1;
+
+% Variables
+alphas = zeros(m, 1);
+b = 0;
+E = zeros(m, 1);
+passes = 0;
+eta = 0;
+L = 0;
+H = 0;
+
+% Pre-compute the Kernel Matrix since our dataset is small
+% (in practice, optimized SVM packages that handle large datasets
+%  gracefully will _not_ do this)
+% 
+% We have implemented optimized vectorized version of the Kernels here so
+% that the svm training will run faster.
+if strcmp(func2str(kernelFunction), 'linearKernel')
+    % Vectorized computation for the Linear Kernel
+    % This is equivalent to computing the kernel on every pair of examples
+    K = X*X';
+elseif strfind(func2str(kernelFunction), 'gaussianKernel')
+    % Vectorized RBF Kernel
+    % This is equivalent to computing the kernel on every pair of examples
+    X2 = sum(X.^2, 2);
+    K = bsxfun(@plus, X2, bsxfun(@plus, X2', - 2 * (X * X')));
+    K = kernelFunction(1, 0) .^ K;
+else
+    % Pre-compute the Kernel Matrix
+    % The following can be slow due to the lack of vectorization
+    K = zeros(m);
+    for i = 1:m
+        for j = i:m
+             K(i,j) = kernelFunction(X(i,:)', X(j,:)');
+             K(j,i) = K(i,j); %the matrix is symmetric
+        end
+    end
+end
+
+% Train
+fprintf('\nTraining ...');
+dots = 12;
+while passes < max_passes,
+            
+    num_changed_alphas = 0;
+    for i = 1:m,
+        
+        % Calculate Ei = f(x(i)) - y(i) using (2). 
+        % E(i) = b + sum (X(i, :) * (repmat(alphas.*Y,1,n).*X)') - Y(i);
+        E(i) = b + sum (alphas.*Y.*K(:,i)) - Y(i);
+        
+        if ((Y(i)*E(i) < -tol && alphas(i) < C) || (Y(i)*E(i) > tol && alphas(i) > 0)),
+            
+            % In practice, there are many heuristics one can use to select
+            % the i and j. In this simplified code, we select them randomly.
+            j = ceil(m * rand());
+            while j == i,  % Make sure i \neq j
+                j = ceil(m * rand());
+            end
+
+            % Calculate Ej = f(x(j)) - y(j) using (2).
+            E(j) = b + sum (alphas.*Y.*K(:,j)) - Y(j);
+
+            % Save old alphas
+            alpha_i_old = alphas(i);
+            alpha_j_old = alphas(j);
+            
+            % Compute L and H by (10) or (11). 
+            if (Y(i) == Y(j)),
+                L = max(0, alphas(j) + alphas(i) - C);
+                H = min(C, alphas(j) + alphas(i));
+            else
+                L = max(0, alphas(j) - alphas(i));
+                H = min(C, C + alphas(j) - alphas(i));
+            end
+           
+            if (L == H),
+                % continue to next i. 
+                continue;
+            end
+
+            % Compute eta by (14).
+            eta = 2 * K(i,j) - K(i,i) - K(j,j);
+            if (eta >= 0),
+                % continue to next i. 
+                continue;
+            end
+            
+            % Compute and clip new value for alpha j using (12) and (15).
+            alphas(j) = alphas(j) - (Y(j) * (E(i) - E(j))) / eta;
+            
+            % Clip
+            alphas(j) = min (H, alphas(j));
+            alphas(j) = max (L, alphas(j));
+            
+            % Check if change in alpha is significant
+            if (abs(alphas(j) - alpha_j_old) < tol),
+                % continue to next i. 
+                % replace anyway
+                alphas(j) = alpha_j_old;
+                continue;
+            end
+            
+            % Determine value for alpha i using (16). 
+            alphas(i) = alphas(i) + Y(i)*Y(j)*(alpha_j_old - alphas(j));
+            
+            % Compute b1 and b2 using (17) and (18) respectively. 
+            b1 = b - E(i) ...
+                 - Y(i) * (alphas(i) - alpha_i_old) *  K(i,j)' ...
+                 - Y(j) * (alphas(j) - alpha_j_old) *  K(i,j)';
+            b2 = b - E(j) ...
+                 - Y(i) * (alphas(i) - alpha_i_old) *  K(i,j)' ...
+                 - Y(j) * (alphas(j) - alpha_j_old) *  K(j,j)';
+
+            % Compute b by (19). 
+            if (0 < alphas(i) && alphas(i) < C),
+                b = b1;
+            elseif (0 < alphas(j) && alphas(j) < C),
+                b = b2;
+            else
+                b = (b1+b2)/2;
+            end
+
+            num_changed_alphas = num_changed_alphas + 1;
+
+        end
+        
+    end
+    
+    if (num_changed_alphas == 0),
+        passes = passes + 1;
+    else
+        passes = 0;
+    end
+
+    fprintf('.');
+    dots = dots + 1;
+    if dots > 78
+        dots = 0;
+        fprintf('\n');
+    end
+    if exist('OCTAVE_VERSION')
+        fflush(stdout);
+    end
+end
+fprintf(' Done! \n\n');
+
+% Save the model
+idx = alphas > 0;
+model.X= X(idx,:);
+model.y= Y(idx);
+model.kernelFunction = kernelFunction;
+model.b= b;
+model.alphas= alphas(idx);
+model.w = ((alphas.*Y)'*X)';
+
+end
+
+
+
+
+
+
+function sim = gaussianKernel(x1, x2, sigma)
+x1 = x1(:); 
+x2 = x2(:);
+
+sim = 0;
+sim = exp(-(sum((x1 - x2) .^ 2)) / (2*(sigma^2)));
+  
+end
+
+
+
+
+
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+C = 1;
+sigma = 0.3;
+
+values = [0.01 0.03 0.1 0.3 1 3 10 30];
+error_min = inf;
+
+for C = values
+  for sigma = values
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    errorCV = mean(double(predictions ~= yval));
+    if (errorCV <= error_min)
+      C_final = C;
+      sigma_final = sigma;
+      error_min = errorCV;
+    end
+  end
+end
+
+C = C_final;
+sigma = sigma_final;
+
+end
+
+
+
+
+
+function word_indices = processEmail(email_contents)
+vocabList = getVocabList();
+
+% Init return value
+word_indices = [];
+
+% Lower case
+email_contents = lower(email_contents);
+
+% Strip all HTML
+email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+
+% Handle Numbers
+email_contents = regexprep(email_contents, '[0-9]+', 'number');
+
+% Handle URLS
+email_contents = regexprep(email_contents, ...
+                           '(http|https)://[^\s]*', 'httpaddr');
+% Handle Email Addresses
+email_contents = regexprep(email_contents, '[^\s]+@[^\s]+', 'emailaddr');
+
+% Handle $ sign
+email_contents = regexprep(email_contents, '[$]+', 'dollar');
+fprintf('\n==== Processed Email ====\n\n');
+
+% Process file
+l = 0;
+
+while ~isempty(email_contents)
+
+    % Tokenize and also get rid of any punctuation
+    [str, email_contents] = ...
+       strtok(email_contents, ...
+              [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+   
+    % Remove any non alphanumeric characters
+    str = regexprep(str, '[^a-zA-Z0-9]', '');
+
+    % Stem the word 
+    % (the porterStemmer sometimes has issues, so we use a try catch block)
+    try 
+      str = porterStemmer(strtrim(str)); 
+    catch 
+      str = ''; 
+      continue;
+    end;
+
+    % Skip the word if it is too short
+    if length(str) < 1
+       continue;
+    end
+
+    
+    for wordindex = 1:length(vocabList)
+      if (strcmp(str, vocabList{wordindex})) == 1
+        word_indices = [word_indices; wordindex];
+      end
+    end
+
+    
+    if (l + length(str) + 1) > 78
+        fprintf('\n');
+        l = 0;
+    end
+    fprintf('%s ', str);
+    l = l + length(str) + 1;
+
+end
+fprintf('\n\n=========================\n');
+end
+
+
+
+
+
+function x = emailFeatures(word_indices)
+% Total number of words in the dictionary
+n = 1899;
+
+% You need to return the following variables correctly.
+x = zeros(n, 1);
+
+for i= word_indices
+  x(i) = 1;
+end
+
+end
+
+
+
+
+
+
+
